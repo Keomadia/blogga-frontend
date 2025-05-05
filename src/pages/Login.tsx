f1eea51c4e5abeb6d3f6ced7e0c56b4d1069d723
Keomadia <keomadianyankah@example.com>
2025-05-05 18:23:40 +0100

    modifiaation made

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 // src/pages/Login.tsx
-import { Container } from '@mui/material';
+import { Container,Box, Typography } from '@mui/material';
 import AuthForm from '../components/AuthForm';
 // import GoogleLogin from '../components/GoogleLogin';
 import DefaultLayout from '../layouts/default';
@@ -10,6 +10,10 @@ export default function Login() {
       <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '20vh 0' }}>
         <AuthForm mode="login" />
         {/* <GoogleLogin /> */}
+        <Box sx={{ marginTop: '20px', textAlign: 'center' }}>
+          <Typography>Don't have an account? <a href="/register">Register</a></Typography>
+          {/* <p>Forgot your password? <a href="/forgot-password">Reset it</a></p> */}
+        </Box>
       </Container>
     </DefaultLayout>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,6 +2,8 @@ import { Container } from '@mui/material';
 import AuthForm from '../components/AuthForm';
 // import GoogleLogin from '../components/GoogleLogin';
 import DefaultLayout from '../layouts/default';
+import { Box, Typography } from '@mui/material';
+
 
 export default function Register() {
   return (
@@ -9,6 +11,11 @@ export default function Register() {
         <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: '20vh 0' }}>
             <AuthForm mode="register" />
             {/* <GoogleLogin /> */}
+
+            <Box sx={{ marginTop: '20px', textAlign: 'center' }}>
+                <Typography>Already have an account? <a href="/login">Login</a></Typography>
+                {/* <p>Forgot your password? <a href="/forgot-password">Reset it</a></p> */}
+              </Box>
         </Container>
     </DefaultLayout>
   );
